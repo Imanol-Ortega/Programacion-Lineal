3af74ee80a65f91c1dfaf2aaf2c9b261dbb601c6
Imanol Ortega <imanicoortemohr018@gmail.com>
2025-02-07 22:02:39 -0300

    resolve error path in import

--- ResolvedorSimplexDual/src/store/simplexStore.ts
+++ ResolvedorSimplexDual/src/store/simplexStore.ts
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import {
     type Inecuacion,
     type FuncionObjetivo,
-} from "@/interfaces/interfaceSimplex";
+} from "../interfaces/interfaceSimplex";
 interface SimplexStoreState {
     funcionObjetivo: FuncionObjetivo;
     inecuaciones: Inecuacion[];
